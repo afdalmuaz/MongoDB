19869d956eb50728873f0d29507db4fb14379a96
afdalmuaz <139734140+afdalmuaz@users.noreply.github.com>
2023-08-28 10:27:17 +0700

    db.js

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const { MongoClient } = require('mongodb');
 
 let dbConnection;
+let uri = 'mongodb+srv://afdalmuaz:uzumakigojo@cluster0.an8ityc.mongodb.net/?retryWrites=true&w=majority'
 
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect('mongodb://127.0.0.1:27017/bookstore')
+    MongoClient.connect(uri)
       .then((client) => {
         dbConnection = client.db();
         return cb();
